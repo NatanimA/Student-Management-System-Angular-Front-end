8cb04b9d6ccbfaa4e4ee337a50a372771df81d2b
NatanimAb <natanim.abesha@jethitech.com>
2023-03-04 23:59:20 +0300

    Insert App routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'students', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path:'', component: HomeComponent},
+  {path:'register', component: RegisterComponent},
+  {path: 'profile/:id', component: ProfileComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
